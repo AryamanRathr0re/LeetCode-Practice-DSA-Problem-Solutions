b5eb9bf118aa0ac0241c9b597b968a951e1a054b
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-01-14 18:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 51.1 MB (55.77%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+
+    let res=[]
+
+    for(let i=0;i<numRows;i++){
+        let row=[]
+        for(let j=0;j<=i;j++){
+            if(j===0||j===i){
+                row.push(1)
+            }
+            else{
+                const num=res[i-1][j]+res[i-1][j-1]
+                row.push(num)
+            }
+        }
+        res.push(row)
+    }
+    return res
+    
+};
