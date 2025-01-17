ff4375dbb64c14a93e6728668a3798d5c1beee3d
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-01-17 22:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 52 MB (63.83%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let str=[]
+    for(let i=1;i<=n;i++){
+        if(i%3===0&&i%5===0)
+       str.push("FizzBuzz")
+        else if(i%3===0)
+       str.push("Fizz")
+        else if(i%5===0)
+       str.push("Buzz")
+        else 
+str.push(i.toString())
+
+    }
+    return str
+};
