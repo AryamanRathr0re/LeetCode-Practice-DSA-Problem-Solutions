f50660e490cdbb9a942e13d6d72af32e64417ddc
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-01-20 22:37:49 +0530

    Time: 57 ms (34.07%) | Memory: 49 MB (53.14%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,12 +4,12 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    // return arr.filter(fn)
-const res=[]
-  for(const i  in arr){
-    if(fn(arr[i],Number(i))){
-        res.push(arr[i])
-    }
-  }
-  return res
+    return arr.filter(fn)
+// const res=[]
+//   for(const i  in arr){
+//     if(fn(arr[i],Number(i))){
+//         res.push(arr[i])
+//     }
+//   }
+//   return res
 };
