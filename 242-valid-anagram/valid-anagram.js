128139607289190add951c94896237f9d37d0637
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-01-24 19:50:22 +0530

    Time: 5 ms (97.95%) | Memory: 50 MB (97.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,16 +4,24 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-  let s1=s.split("").sort().join("")
-  let t1=t.split("").sort().join("")
 
-  if(s1===t1){
+    if(s.length!==t.length){
+        return false
+    }
+
+    let a1=new Array(128).fill(0)
+    let a2=new Array(128).fill(0)
+
+    for(let i=0;i<s.length;i++){
+        a1[s.charCodeAt(i)]++
+        a2[t.charCodeAt(i)]++
+    }
+
+    for(let i=0;i<a1.length;i++){
+        if(a1[i]!==a2[i]){
+            return false
+        }
+
+    }
     return true
-  }
-  else{
-    return false
-  }
-
-
-
 };
