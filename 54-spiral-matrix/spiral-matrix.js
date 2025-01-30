7411c9ffa8b46a028fb969b81e531f1c95549bbb
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-01-30 22:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 49 MB (45.53%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.js
+++ 54-spiral-matrix/spiral-matrix.js
@@ -13,32 +13,32 @@ var spiralOrder = function(matrix) {
     let count=0
     let total=n*m
 
-    while(count<total){
-
-        for(let i=left;i<=right&&count<total;i++){
-            res.push(matrix[top][i])
-            count++
-        }
-        top++
-
-        for(let i=top;i<=bottom&&count<total;i++){
-            res.push(matrix[i][right])
-            count++
-        }
-        right--
-
-        for(let i=right;i>=left&&count<total;i--){
-            res.push(matrix[bottom][i])
-            count++
-        }
-        bottom--
-
-        for(let i=bottom;i>=top&&count<total;i--){
-            res.push(matrix[i][left])
-            count++
-        }
-        left++
+    while (count < total) {
+    for (let i = left; i <= right && count < total; i++) {
+      res.push(matrix[top][i]);
+      count++;
     }
+    top++;
+
+    for (let i = top; i <= bottom && count < total; i++) {
+      res.push(matrix[i][right]);
+      count++;
+    }
+    right--;
+
+    for (let i = right; i >= left && count < total; i--) {
+      res.push(matrix[bottom][i]);
+      count++;
+    }
+    bottom--;
+
+    for (let i = bottom; i >= top && count < total; i--) {
+      res.push(matrix[i][left]);
+      count++;
+    }
+    left++;
+  }
+
 return res
 
 
