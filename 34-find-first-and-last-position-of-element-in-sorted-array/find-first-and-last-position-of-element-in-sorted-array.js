c4ac0ac94fc2ed5a1353ab3d73191498b193b9a3
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-04-10 13:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (23.43%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,11 +7,11 @@
 function rightMost(nums,target,n){
     let start=0
     let end=n-1
-    let rightMost=-1
+   let  right=-1
     while(start<=end){
         let mid=Math.floor(start+(end-start)/2)
         if(nums[mid]===target){
-            rightMost=mid
+            right=mid
             start=mid+1
         }
         else if(nums[mid]<target){
@@ -21,18 +21,18 @@ function rightMost(nums,target,n){
             end=mid-1
         }
     }
-    return rightMost
-}
+     return right
 
+}
 
 function leftMost(nums,target,n){
     let start=0
     let end=n-1
-    let leftMost=-1
+ let   left=-1
     while(start<=end){
         let mid=Math.floor(start+(end-start)/2)
         if(nums[mid]===target){
-            leftMost=mid
+            left=mid
             end=mid-1
         }
         else if(nums[mid]<target){
@@ -42,8 +42,10 @@ function leftMost(nums,target,n){
             end=mid-1
         }
     }
-    return leftMost
+     return left
+
 }
+
 var searchRange = function(nums, target) {
     let n=nums.length
     let left_most=leftMost(nums,target,n)
