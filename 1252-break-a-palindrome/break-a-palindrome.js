a558dd1b7819936d7af520c16cf9e833b2a83331
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-05-15 12:32:38 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (41.30%) - LeetSync

--- 1252-break-a-palindrome/break-a-palindrome.js
+++ 1252-break-a-palindrome/break-a-palindrome.js
@@ -3,18 +3,16 @@
  * @return {string}
  */
 var breakPalindrome = function(palindrome) {
-    let n = palindrome.length;
-    if (n === 1) return "";
-    
-    let arr = palindrome.split('');
-    
-    for (let i = 0; i < Math.floor(n / 2); i++) {
-        if (arr[i] !== 'a') {
-            arr[i] = 'a';
-            return arr.join('');
+    let n=palindrome.length
+
+    if(n===1) return ""
+    let arr=palindrome.split('')
+    for(let i=0;i<Math.floor(n/2);i++){
+        if(arr[i]!=="a"){
+            arr[i]="a"
+            return arr.join("")
         }
     }
-    
-    arr[n - 1] = 'b';
-    return arr.join('');
+    arr[n-1]='b'
+    return arr.join('')
 };
