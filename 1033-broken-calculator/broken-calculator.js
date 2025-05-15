237b9ad5adfa8f0acb6de102fbbbb96962474292
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-05-15 12:50:39 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (32.00%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} startValue
+ * @param {number} target
+ * @return {number}
+ */
+var brokenCalc = function(startValue, target) {
+    if(startValue>=target){
+        return startValue-target
+    }
+    if(target%2===0){
+        return 1+ brokenCalc(startValue,target/2)
+
+    }
+        return 1+ brokenCalc(startValue,target+1)
+
+};
