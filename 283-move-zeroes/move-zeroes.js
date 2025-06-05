cf354e2a6f68d2050b03e3211c38607943182338
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-06-05 13:37:17 +0530

    Time: 1 ms (81.08%) | Memory: 60.8 MB (33.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,13 +3,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-     let count=0
-
+    let x=0
     for(let i=0;i<nums.length;i++){
         if(nums[i]!==0){
-            [nums[count],nums[i]]=[nums[i],nums[count]]
-            count++
+            nums[x]=nums[i]
+            x++
         }
+    }
+    for(let i=x;i<nums.length;i++){
+        nums[i]=0
     }
     return nums
 };
