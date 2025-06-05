2fef2b283f90e0a193a7024b0fb619eb96a0b5e0
AryamanRathr0re <105965903+AryamanRathr0re@users.noreply.github.com>
2025-06-05 12:56:32 +0530

    Time: 0 ms (100.00%) | Memory: 55.7 MB (34.08%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let n1Copy=nums1.slice(0,m)
+    let p1=0
+    let p2=0
+    for(let i=0;i<m+n;i++){
+        if(p2>=n||(n1Copy[p1]<nums2[p2] && p1<m)){
+            nums1[i]=n1Copy[p1]
+            p1++
+        }
+        else{
+            nums1[i]=nums2[p2]
+            p2++
+        }
+    }
+};
