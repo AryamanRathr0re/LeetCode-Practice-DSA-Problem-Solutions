8f489108a209026b2493c7fe193775b5a7d484c9
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2025-06-15 05:07:22 +0530

    Time: 60 ms (46.91%) | Memory: 63.8 MB (49.83%) - LeetSync

---
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let sett=new Set()
+    while(headB!=null){
+        sett.add(headB)
+        headB=headB.next
+    }
+    while(headA){
+        if(sett.has(headA)) return headA
+
+        headA=headA.next
+    }
+    return null
+};
