ab94a0c7cc1cdb2782c82d38ae4c3e28ada1979d
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2025-06-16 04:23:55 +0530

    Time: 0 ms (100.00%) | Memory: 58.9 MB (45.66%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.js
+++ 328-odd-even-linked-list/odd-even-linked-list.js
@@ -10,17 +10,18 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    if(head===null||head.next===null){
+    if(!head||!head.next){
         return head
     }
     let odd=head
-    let even= head.next
-    let evenstart=head.next
-    while(even!==null&&even.next!==null){
-        odd.next=even.next
+    let even=head.next
+    let evenstart=even
+    while(odd.next&&even.next){
+        odd.next=odd.next.next
         even.next=even.next.next
         odd=odd.next
         even=even.next
+
     }
     odd.next=evenstart
     return head
