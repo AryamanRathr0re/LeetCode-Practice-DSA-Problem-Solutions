08e84661cfacb8904b78ba4971b84e3d3a329253
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2025-06-18 17:14:16 +0530

    Time: 1 ms (82.56%) | Memory: 57.9 MB (56.52%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -6,9 +6,12 @@
 var findWordsContaining = function(words, x) {
     let arr=[]
     for(let i=0;i<words.length;i++){
-        if(words[i].includes(x)){
+       for(let j=0;j<words[i].length;j++){
+        if(words[i][j]===x){
             arr.push(i)
+            break
         }
+       }
     }
     return arr
 };
