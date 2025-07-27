ae14ad78ef28a8d91ded1ad3956d208179adc2ac
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2025-07-27 09:19:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (32.31%) - LeetSync

---
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function countHillValley(nums) {
+    let res = 0;
+    for (let i = 1, prev = nums[0], n = nums.length - 1; i < n; i++) {
+        if (
+            prev < nums[i] && nums[i] > nums[i + 1] || // hill
+            prev > nums[i] && nums[i] < nums[i + 1]    // valley
+        ) res++, prev = nums[i];
+    }
+    return res;
+}
