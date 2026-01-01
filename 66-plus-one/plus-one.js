a9de5273391f032b56536af529b351864eec86d3
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-01-01 11:04:03 +0530

    Time: 1 ms (4.59%) | Memory: 52.7 MB (97.31%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -3,21 +3,13 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for(let i=digits.length-1;i>=0;i--){
-  
-  if(digits[i]!==9){
-    digits[i]=digits[i]+1
-    break
-  }
-  
-  if(digits[i]===9){
-    digits[i]=0
-  }
-  if(i===0&&digits[i]===0){
-    digits.unshift(1)
-  }
-
-}
-return digits
-    
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
 };
