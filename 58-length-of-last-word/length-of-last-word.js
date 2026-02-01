d9a23b2293cdd710ba3d6d423cf5daa4b9785cb4
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-02-01 19:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (23.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,15 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s=s.trim()
-    let count=0
-    for(let i=s.length-1;i>=0;i--){
-        if(s[i]!==" "){
-            count++
-        }
-        else{
-            break
-        }
-    }
-    return count
+   let str=s.trim()
+   return str.split(" ").pop().length  
 };
