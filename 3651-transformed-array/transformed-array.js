27190646c167cb4fd3262af46da062b8108ef50b
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-02-05 18:50:23 +0530

    Time: 61 ms (25.00%) | Memory: 58.5 MB (9.38%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var constructTransformedArray = function (nums) {
+    let n = nums.length;
+    if (n == 1) return [nums[0]];
+    const res = [];
+    for (let i = 0; i < n; i++) {
+        let idx = (i + 100 * n + nums[i]) % n;
+        res.push(nums[idx]);
+    }
+    return res;
+};
