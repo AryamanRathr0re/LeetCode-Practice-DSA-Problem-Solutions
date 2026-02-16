fd088d3430e156372ec3e821a0b0c42dd471a7e1
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-02-16 16:49:23 +0530

    Time: 2 ms (82.02%) | Memory: 64.3 MB (94.97%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -4,14 +4,14 @@
  */
 var maxProfit = function(prices) {
     let min=prices[0]
-    let maxProfit=0
+    let maxP=0
     for(let i=1;i<prices.length;i++){
-        if(prices[i]-min>maxProfit){
-            maxProfit=prices[i]-min
+        if(prices[i]-min>maxP){
+            maxP=prices[i]-min
         }
-        if(min>prices[i]){
+        if(prices[i]<min){
             min=prices[i]
         }
     }
-    return maxProfit
+    return maxP
 };
