b89e060569556fda1bde155e107d4123589c0cfd
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-03-06 21:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (16.67%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -0,0 +1,13 @@
+var checkOnesSegment = function(s) {
+    let seenZero = false;
+
+    for (let c of s) {
+        if (c === '0') {
+            seenZero = true;
+        } else if (seenZero) {
+            return false;
+        }
+    }
+
+    return true;
+};
