773f5935c831ea20769e53d4ef0148952e8168f6
Aryaman Rathore <105965903+AryamanRathr0re@users.noreply.github.com>
2026-05-04 23:59:36 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (59.87%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let n=matrix.length
+    let mat=   Array.from({ length: n }, () => Array(n).fill(0));
+
+    for(let j=0;j<n;j++){
+      
+        for(let i =n-1;i>=0;i--){
+            mat[j][n-1-i]=matrix[i][j]
+            
+        }
+        
+    }
+    for(let i=0;i<n;i++){
+        for (let j=0;j<n;j++){
+            matrix[i][j]=mat[i][j]
+        }
+    }
+   
+};
